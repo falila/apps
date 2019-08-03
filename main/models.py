b984b49e00ce497bdbfbb97a613f8e5a56082aae
falila <soulfa23@gmail.com>
2019-08-03 18:48:16 -0400

    driver custom function

--- main/models.py
+++ main/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-from restaurant.models import Restaurant
-
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer', null=True)
@@ -11,7 +9,7 @@ class Customer(models.Model):
     avatar = models.ImageField(upload_to='customer_avatar/', blank=True)
     phone = models.CharField(max_length=500, blank=True)
     address = models.CharField(max_length=500, blank=True)
-    fip = models.CharField(max_length=10,blank=True)
+    fip = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return self.user.get_full_name()
@@ -24,7 +22,7 @@ class Driver(models.Model):
     phone = models.CharField(max_length=45, blank=True)
     address = models.CharField(max_length=500, blank=True)
     location = models.CharField(max_length=150, blank=True)
-    fip = models.CharField(max_length=10,blank=True)
+    fip = models.CharField(max_length=10, blank=True)
     lon = models.FloatField(blank=True, default=0)
     lat = models.FloatField(blank=True, default=0)
 
--- main/views.py
+++ main/views.py
@@ -1,18 +1,23 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
+from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from oauth2_provider.models import AccessToken
 from rest_framework import permissions, status, viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from main import serializers
 from main.models import Tag, Driver, Customer
-from main.serializers import UserSerializer
+from main.serializers import UserSerializer, OrderSerializer
+from order.models import Order
 
 
 def index(request):
@@ -94,6 +99,123 @@ class DriverViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.order_by('-name')
+
+    # POST params: access_token, order_id
+    @action(detail=False, methods=['post'])
+    def driver_pick_orders(request):
+        if request.method == "POST":
+            # Get token
+            if request.user:
+                driver = request.user.driver
+
+            # Check if the driver can only pick up one order at the same timezone
+            if Order.objects.filter(driver=driver).exclude(status=Order.ONTHEWAY):
+                return JsonResponse({"status": "failed", "error": "You can only pick up one order at a time."})
+
+            try:
+                order = Order.objects.get(
+                    id=request.POST["order_id"],
+                    driver=None,
+                    status=Order.READY
+
+                )
+                order.driver = driver
+                order.status = Order.ONTHEWAY
+                order.picked_at = timezone.now()
+                order.save()
+
+                return JsonResponse({"status": "success"})
+
+            except Order.DoesNotExist:
+                return JsonResponse({"status": "failed", "error": "This order has been picked up by another driver."})
+
+    # GET params: access_token
+    @action(detail=False, methods=['get'])
+    def driver_get_latest_orders(self, request):
+        if request.method == "GET" and request.user:
+            try:
+                driver = request.user.driver
+                if not driver:
+                    raise Exception
+
+                order = OrderSerializer(
+                    Order.objects.get.filter(driver=driver).order_by("picked_at").last()
+                ).data
+            except Exception as e:
+                return JsonResponse({"status": "failed", "error": "No driver found."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+        return JsonResponse({"order": order})
+
+    # POST params: access_token, order_id
+    @action(detail=False, methods=['post'])
+    def driver_complete_orders(self, request):
+        if request.method == "POST" and request.user:
+            try:
+                driver = request.user.driver
+                if not driver:
+                    raise Exception
+            except Exception as e:
+                return JsonResponse({"status": "failed", "error": "No driver found."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+        order = Order.objects.get(id=request.POST["order_id"], driver=driver)
+        order.status = order.DELIVERED
+        order.save()
+
+        return JsonResponse({"status": "success"})
+
+    # GET params: access_token
+    @action(detail=False, methods=['get'])
+    def driver_get_revenue(self, request):
+        if request.user:
+            try:
+                driver = request.user.driver
+                if not driver:
+                    raise Exception
+            except Exception as e:
+                return JsonResponse({"status": "failed", "error": "No driver found."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+        revenue = {}
+        today = timezone.now()
+        current_weekdays = [today + timedelta(days=i) for i in range(0 - today.weekday(), 7 - today.weekday())]
+
+        for day in current_weekdays:
+            orders = Order.objects.filter(
+                driver=driver,
+                status=Order.DELIVERED,
+                created_at__year=day.year,
+                created_at__month=day.month,
+                created_at__day=day.day
+            )
+
+            revenue[day.strtime("%a")] = sum(order.total for order in orders)
+
+        return JsonResponse({"revenue": revenue})
+
+    # POST params: access_token, "lat, long"
+    @action(detail=False, methods=['post'])
+    def driver_update_location(self, request):
+        if request.method == "POST":
+            try:
+                driver = request.user.driver
+                if not driver:
+                    raise Exception
+            except Exception as e:
+                return JsonResponse({"status": "failed", "error": "No driver found."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+            # SET location string => database
+            driver.location = request.POST["location"]
+            driver.save()
+            return JsonResponse({"status": "success"})
+
+    @action(detail=False, methods=['get'])
+    def driver_get_ready_orders(self, request):
+        order = OrderSerializer(
+            Order.objects.filter(status=Order.READY, driver=None).order_by("-id"),
+            many=True).data
+        return JsonResponse({"orders": order})
 
 
 class CustomerViewSet(viewsets.GenericViewSet,
